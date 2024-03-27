3495ccedd4acc40130b2746abc699c95093c4749
Mathieu Triay <mathieu.triay@gmail.com>
2024-03-27 17:57:44 +0000

    Manually configure Next

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,21 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let pageConfig = {}
+
+if (isGithubActions) {
+	pageConfig = {
+		basePath: '/synthetic-voice-research',
+		images: {
+			unoptimized: true
+		},
+		output: 'export'
+	}
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	...pageConfig,
 	eslint: {
 		// Warning: This allows production builds to successfully complete even if
 		// your project has ESLint errors.
